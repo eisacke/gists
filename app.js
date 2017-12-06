2826f0f7e2220b66f2035c9dc2661e0d05c91e3a
eisacke <emily@isacke.com>
2017-12-06 13:05:51 +0000

    Testing focus

--- app.js
+++ app.js
@@ -78,15 +78,22 @@ Pad.Carousel = {
 
     console.log(this.$inputs);
 
-    this.$inputs.focus(function() {
-      console.log('focus');
-      this.userFocused = true;
-    }.bind(this));
+    // this.$inputs.focus(function() {
+    //   console.log('focus');
+    //   this.userFocused = true;
+    // }.bind(this));
 
     this.$inputs.blur(function() {
       console.log('blur');
       this.userFocused = false;
     }.bind(this));
+
+    for(let i=0; i < this.$inputs.length; i++) {
+      this.$inputs.get(i).addEventListener('focus', function() {
+        console.log('focus');
+        this.userFocused = true;
+      }.bind(this));
+    }
 
     $(window).on('keypress', function(e) { // go to next slide if user presses enter whilst focused on an input
       console.log('keypress', e.keyCode);
